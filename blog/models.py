6228187e72c611850bdba75422366863ec3982cf
Minjae <rjsekaehdhkw@gmail.com>
2022-05-31 10:36:58 +0900

    DjangoQuiz 5번까지 완료

--- blog/models.py
+++ blog/models.py
@@ -7,14 +7,14 @@ class ArticleModel(models.Model):
     class Meta:
         db_table = "Article"
 
-    title = models.CharField(max_length=20, null=False)
-    category = models.CharField(max_length=256, null=False)
-    content = models.ForeignKey("CategoryModel", max_length=256, null=False, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    category = models.ForeignKey("CategoryModel", on_delete=models.CASCADE)
 
 
 class CategoryModel(models.Model):
     class Meta:
         db_table = "Category"
 
-    name = models.CharField(max_length=20, null=False)
-    explanation = models.CharField(max_length=256, default='')
+    name = models.CharField(max_length=256,)
+    explanation = models.TextField()
